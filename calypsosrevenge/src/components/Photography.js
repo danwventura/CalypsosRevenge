3869b460436c7cea6cc25f2ba276d87dafe7f045
danwventura <daniel.w.ventura@vumc.org>
2024-01-07 15:50:59 -0600

    Create basic component structure

---
+++ calypsosrevenge/src/components/Photography.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Photography(){
+    return(
+        <h5>Photography Here</h5>
+    )
+}
---
+++ calypsosrevenge/src/components/Videos.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Videos (){
+
+    return(
+        <h5>Videos Here!</h5>
+    )
+}
