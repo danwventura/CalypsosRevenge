eca7c57a1b6be8da5dfc53af840d136e46a4c1bd
danwventura <daniel.w.ventura@vumc.org>
2024-01-27 14:37:31 -0600

    Creating basic styling for photo gallery page

--- calypsosrevenge/app/page.tsx
+++ calypsosrevenge/app/page.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="text-gray-400 bg-gray-900 body-font">
       <NavBar />
     </main>
   )
