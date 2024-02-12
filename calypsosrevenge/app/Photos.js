64a1f2fb1da7c3e305204ca352987d9d09256be1
danwventura <daniel.w.ventura@vumc.org>
2024-02-11 20:23:07 -0600

    Adding and displaying photos

---
+++ calypsosrevenge/app/Photos.js
@@ -0,0 +1,26 @@
+export const photos = [
+    {
+        title: "duo1",
+        subtitle: "subtitle text 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        image: "/img/duo1.PNG",
+    },
+    {
+        title: "duo1",
+        subtitle: "subtitle text 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        image: "/img/duo2.jpg",
+    },
+    {
+        title: "duo1",
+        subtitle: "subtitle text 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        image: "/img/duo3.jpg",
+    },
+    {
+        title: "duo1",
+        subtitle: "subtitle text 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        image: "/img/solo1.JPEG",
+    }
+]
--- calypsosrevenge/app/about/page.jsx
+++ calypsosrevenge/app/about/page.jsx
@@ -6,6 +6,9 @@ export default function About () {
             <section id="about">
                 <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 itemsçenter text-center">
+                        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 items-center justify-center">
+                            <img className="object-cover object-center rounded" alt="hero" src="/img/logo.jpg" />
+                        </div>
                         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                             Hi, Im Lauren
                             <br className="hidden lg:inline-block" /> I love making music!
