db7f53f3b64fd9e73460a46946b27a09764ee467
danwventura <daniel.w.ventura@vumc.org>
2024-02-17 13:44:51 -0600

    Breaking out images from dynamic elements for more custom styling

--- calypsosrevenge/app/photography/page.jsx
+++ calypsosrevenge/app/photography/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, {Component} from "react";
 import NavBar from "../NavBar";
-import {photos} from "../Photos";
+import {duo1} from "../Photos";
 
 export default function Photography () {
         return(
@@ -20,27 +20,79 @@ export default function Photography () {
                             fuga dolore.
                         </p>
                     </div>
-                    <div className="flex flex-wrap -m-4">
-                        {photos.map((photo)=> (
-                            <a href={photo.link} key={photo.image} className="sm:w-1/2 w-100 p-4">
+                    <div className="flex flex-wrap h-full">
+                            <a href="/img/duo1.PNG" className="lg:w-1/2 w-100 p-8">
                                 <div className="flex relative">
                                     <img 
                                         alt="gallery" 
-                                        className="absolute inset-0 w-full h-full object-cover object-center"
-                                        src={photo.image}
+                                        className="absolute inset-0 w-full h-full"
+                                        src="/img/duo1.PNG"
                                      />
                                     <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                                         <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                                            {photo.subtitle}
+                                            "Subtitle 1"
                                         </h2>
                                         <h1 className="title-font text-large font-medium text-white mb-3">
-                                            {photo.title}
+                                            "Title 1"
                                         </h1>
-                                        <p>{photo.description}</p>
+                                        <p>"Description 1"</p>
                                     </div>
                                 </div>
                             </a>
-                         ))}
+                            <a href="/img/duo2.jpg" className="lg:w-1/2 w-100 p-8">
+                                <div className="flex relative">
+                                    <img 
+                                        alt="gallery" 
+                                        className="absolute inset-0 w-full h-full"
+                                        src="/img/duo2.jpg"
+                                     />
+                                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                                        <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                                            "Subtitle 2"
+                                        </h2>
+                                        <h1 className="title-font text-large font-medium text-white mb-3">
+                                            "Title 2"
+                                        </h1>
+                                        <p>"Description 2"</p>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/img/duo3.jpg" className="lg:w-1/2 w-100 p-8">
+                                <div className="flex relative">
+                                    <img 
+                                        alt="gallery" 
+                                        className="absolute inset-0 w-full h-full"
+                                        src="/img/duo3.jpg"
+                                     />
+                                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                                        <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                                            "Subtitle 3"
+                                        </h2>
+                                        <h1 className="title-font text-large font-medium text-white mb-3">
+                                            "Title 3"
+                                        </h1>
+                                        <p>"Description 3"</p>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/img/solo1.JPEG" className="lg:w-1/2 w-100 p-8">
+                                <div className="flex relative">
+                                    <img 
+                                        alt="gallery" 
+                                        className="absolute inset-0 w-full h-full"
+                                        src="/img/solo1.JPEG"
+                                     />
+                                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                                        <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                                            "Subtitle 4"
+                                        </h2>
+                                        <h1 className="title-font text-large font-medium text-white mb-3">
+                                            "Title 4"
+                                        </h1>
+                                        <p>"Description 4"</p>
+                                    </div>
+                                </div>
+                            </a>
                     </div>
                 </div>
             </section>
