229cd8bbfb7c2abde9d391b30fbfa270c1aff2ae
danwventura <daniel.w.ventura@vumc.org>
2024-03-03 17:56:40 -0600

    Repositioning about section

--- calypsosrevenge/app/about/page.jsx
+++ calypsosrevenge/app/about/page.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 
 export default function About () {
         return(
-            <section class="bg-gradient-to-r from-calypsoBlue to-calypsoSky h-screen">
+            <section class="bg-gradient-to-r from-calypsoBlue to-calypsoSky mt-32 h-screen">
                 <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div class="mr-auto place-self-center lg:col-span-7">
                         <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">This is <br/>Calypso's Revenge</h1>
