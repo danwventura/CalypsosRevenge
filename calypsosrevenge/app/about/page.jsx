cdb3f21487b76baeaa2cfe4eec235377100fe8e4
danwventura <daniel.w.ventura@vumc.org>
2024-03-09 00:12:52 -0600

    Adding image carousel and basic styling for first version

--- calypsosrevenge/app/about/page.jsx
+++ calypsosrevenge/app/about/page.jsx
@@ -3,11 +3,11 @@ import React, {Component} from "react";
 
 export default function About () {
         return(
-            <section class="bg-gradient-to-r from-calypsoBlue to-calypsoSky mt-32 h-screen">
+            <section class="bg-gradient-to-r from-calypsoBlue to-calypsoSky mt-20 h-screen">
                 <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div class="mr-auto place-self-center lg:col-span-7">
-                        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">This is <br/>Calypso's Revenge</h1>
-                        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl text-white-400">Two singer-songwriters sharing their music all across Delaware.</p>
+                        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">This is <br/>Calypso&#39;s Revenge</h1>
+                        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl text-white-400">Two soulful singers sharing their music all across Delaware.</p>
                         <a href="#booking" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg bg-white hover:bg-transparent hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                             Book Me
                         </a> 
