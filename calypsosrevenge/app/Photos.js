424cbf06f7607653f6ed40595bab9ce6a027b9c1
Ventura <daniel.w.ventura@vumc.org>
2024-04-15 22:10:13 -0500

    Adding more images. Fixing about us photos. Fixing carousel display.

--- calypsosrevenge/app/Photos.js
+++ calypsosrevenge/app/Photos.js
@@ -3,19 +3,31 @@ export const photos = [
         title: "duo1",
         subtitle: "subtitle text 1",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        location: "/img/duo1.PNG",
+        location: "/img/duo1.jpg",
     },
     {
         title: "duo2",
         subtitle: "subtitle text 1",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        location: "/img/duo2.png",
+        location: "/img/duo2.jpg",
+    },
+    {
+        title: "duo3",
+        subtitle: "subtitle text 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        location: "/img/duo3.jpg",
     },
     {
         title: "duo4",
         subtitle: "subtitle text 1",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         location: "/img/duo4.jpg",
+    },
+    {
+        title: "solo2",
+        subtitle: "subtitle text 1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        location: "/img/solo2.jpg",
     }
     // {
     //     title: "duo1",
