a140a58a3509fc014dcbf616ce5f06c6fa72feb0
danwventura <danwventura@gmail.com>
2024-06-25 12:05:47 -0400

    Adding text blurb under Bad Idea for Mashup

--- calypsosrevenge/app/covers/page.tsx
+++ calypsosrevenge/app/covers/page.tsx
@@ -28,6 +28,27 @@ export default function Covers () {
                             </div>
                         </div>
                     </div>
+                    <div className="grid grid-cols-1 gap-4 pb-8 mt-20">
+                        <div className="flex text-left items-center font-about font-bold text-white">
+                            <div>
+                                <p>Calypso&#39;s Revenge is a collaboration of two artists, generations apart in age, but with a nearly identical vision of what is interesting and emotionally moving in music.   This track is a perfect example of Lauren and Joe&#39;s collaborative energies and tastes, and is the result of two artists working together to create their own unique take on music across genres.</p>
+                                <p>&nbsp;</p>
+                                <p>Like Eurythmics, Lauren and Joe start with a powerful lead vocalist with in-your-face passion and talent.  Then, the wizard behind the curtain works to create a magical, theatrical sound experience to move things to the next level.   Essentially approaching every song they play as an acoustic duo, some songs remain very “bare-bones” and sparsely orchestrated in order to get the full impact of Lauren&#39;s amazing vocals: One voice, one guitar.  
+                                   Others use a full palette and pull out all the stops as this one does. </p>
+                                <p>&nbsp;</p>
+                                <p>As a band, Calypso&#39;s Revenge may use a drum machine, a looper, and at times, samples to enhance their music.  With this track every tool is implemented.   There really is so much going on, that this performance takes two wizards behind the curtain with both Joe and Lauren flicking switches and stomping pedals.</p>
+                                <p>&nbsp;</p>
+                                <p>“Bad Idea for a Mashup” began as most Calypso&#39;s Revenge tunes do, with Lauren presenting Joe with an idea or tune that interests her, hoping it will appeal to him in some way or spark his creativity.   More than 90% of the time, it does.  For this track, that song was Olivia Rodrigo&#39;s “Bad Idea, Right?” The song itself is built around a main riff that is identical to “Midnight Hour” by Wilson Pickett.  Well, why not stick the two of those songs together?  And so, it began.</p>
+                                <p>&nbsp;</p>
+                                <p>The choruses of “Bad Idea” have a drone loop similar to The Who&#39;s “Baba O&#39;Riley,” so they definitely had to add that, and you will hear the actual sample from that track throughout the mashup. Once that came, ultimately their Frankenstein was created.  This is typical of the work they do together. </p>
+                                <p>&nbsp;</p>
+                                <p>Some interesting details about this mashup: there are two drum tracks, one from the Alesis-16, an old perennial in the music business, and one from the Rodrigo song. 
+                                   Both continue and play as one throughout. You may also note that some background vocals are sampled and layered with Joe&#39;s vocal tracks. </p>
+                                <p>&nbsp;</p>
+                                <p>While not a complete analysis, we hope to have provided a little behind-the-scenes peek into the warped minds of Lauren and Joe and how they go about creating what is Calypso&#39;s Revenge.  Enjoy!</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         )
